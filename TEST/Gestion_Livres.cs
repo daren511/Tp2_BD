b0307e0028992c65711b21791509215d29e52d4b
Daren-Ken St-laurent <daren511@hotmail.com>
2014-03-14 16:07:36 -0400

    should work

---
+++ TEST/Gestion_Livres.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+
+namespace St_laurent_Daren_ken_ET_Cote_Francis_Tp2
+{
+    public partial class Gestion_Livres : Form
+    {
+        public OracleConnection conn = null;
+        public Form callBackForm = null;
+        private DataSet livreDataSet = null;
+        public Gestion_Livres()
+        {
+            InitializeComponent();
+        }
+
+        private void BTN_Close_Click(object sender, EventArgs e)
+        {
+            if (callBackForm != null)
+                callBackForm.Show();
+            this.Close();
+
+        }
+
+        private void BTN_Ajout_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BTN_Modifier_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void reloadDGV()
+        {
+            int lastIndex = -1;
+            if (DGV_Livres.SelectedRows.Count > 0) lastIndex = DGV_Livres.SelectedRows[0].Index;
+
+            OracleCommand oraSelect = conn.CreateCommand();
+            oraSelect.CommandText = "GestionLivres.Consultlivre";
+            oraSelect.CommandType = CommandType.StoredProcedure;
+
+            OracleParameter oraParaResult = new OracleParameter("Resultats", OracleDbType.RefCursor);
+            oraParaResult.Direction = ParameterDirection.ReturnValue;
+            oraSelect.Parameters.Add(oraParaResult);
+
+            OracleDataAdapter oraAdapter = new OracleDataAdapter(oraSelect);
+            livreDataSet = new DataSet();
+            oraAdapter.Fill(livreDataSet);
+            DGV_Livres.DataSource = livreDataSet.Tables[0];
+
+            if (lastIndex > -1 && DGV_Livres.Rows.Count > 0) DGV_Livres.Rows[Math.Min(lastIndex, DGV_Livres.Rows.Count - 1)].Selected = true;
+
+            updateControls();
+        }
+        private void updateControls()
+        {
+            if (DGV_Livres.RowCount > 0)
+            {
+                BTN_Modifier.Enabled = true;
+                BTN_Delete.Enabled = true;
+            }
+            else
+            {
+                BTN_Modifier.Enabled = false;
+                BTN_Delete.Enabled = false;
+            }
+        }
+
+        private void BTN_Delete_Click(object sender, EventArgs e)
+        {
+            DialogResult Confirmation;
+            Confirmation = MessageBox.Show("Voulez-vous vraiment effacer cette entrée ?", "Confirmation", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (Confirmation == System.Windows.Forms.DialogResult.OK)
+            {
+                try
+                {
+                    OracleCommand oraDelete = new OracleCommand("GestionLivres", conn);
+                    oraDelete.CommandText = "GestionLivre.Supprimer";
+                    oraDelete.CommandType = CommandType.StoredProcedure;
+
+                    OracleParameter oraParaNumLivre = new OracleParameter("PNumLivre", OracleDbType.Int32);
+                    oraParaNumLivre.Value = DGV_Livres.SelectedRows[0].Cells[0].Value.ToString();
+                    oraParaNumLivre.Direction = ParameterDirection.Input;
+                    oraDelete.Parameters.Add(oraParaNumLivre);
+
+                    oraDelete.ExecuteNonQuery();
+                    reloadDGV();
+                }
+                catch (OracleException ex)
+                {
+                    if (ex.Number == 2292)
+                    {
+                        MessageBox.Show("TBD", "Erreur 2292", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+
+            }
+        }
+
+        private void Gestion_Livres_Load(object sender, EventArgs e)
+        {
+            reloadDGV();
+        }
+
+        private void Gestion_Livres_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(callBackForm != null)
+               callBackForm.Show();
+            
+        }
+    }
+}
---
+++ TEST/Login.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+
+namespace St_laurent_Daren_ken_ET_Cote_Francis_Tp2
+{
+    public partial class Login : Form
+    {
+        private OracleConnection conn = null;
+        public Login()
+        {
+            InitializeComponent();
+            UpdateControls();
+        }
+
+
+        private void BTN_Quitter_Click(object sender, EventArgs e)
+        {
+            if (conn != null)
+                conn.Close();
+            this.Close();
+        }
+
+        private void BTN_Connect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Cursor = Cursors.WaitCursor;
+                string Dsource = "(DESCRIPTION="
+               + "(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)"
+               + "(HOST=205.237.244.251)(PORT=1521)))"
+               + "(CONNECT_DATA=(SERVICE_NAME=ORCL)))";
+
+                String ChaineConnexion = "Data Source=" + Dsource
+                + ";User Id=" + TB_Username.Text + " ; Password =" + TB_Password.Text;
+                conn = new OracleConnection();
+                conn.ConnectionString = ChaineConnexion;
+
+                conn.Open();
+
+                if (conn.State.ToString() == "Open")
+                {
+                    Cursor = Cursors.Default;
+                    Gestion_Livres fd = new Gestion_Livres();
+                    fd.conn = conn;
+
+                    this.Hide();
+                    fd.callBackForm = this;
+                    fd.Show();
+                    LB_Error.Text = "";
+                    LB_MDP.ForeColor = Color.Black;
+                    LB_Username.ForeColor = Color.Black;
+                }
+
+            }
+            catch (OracleException ex)
+            {
+                ErrorMessage(ex);
+                Cursor = Cursors.Default;
+            }
+        }
+
+        private void TB_Password_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                BTN_Connect_Click(sender, e);
+            }
+        }
+
+        private void UpdateControls()
+        {
+            if (TB_Username.Text == "" || TB_Password.Text == "")
+            {
+                BTN_Connect.Enabled = false;
+            }
+            else
+            {
+                BTN_Connect.Enabled = true;
+            }
+        }
+
+        private void TB_TextChanged(object sender, EventArgs e)
+        {
+            UpdateControls();
+        }
+        private void ErrorMessage(OracleException Ex)
+        {
+            switch (Ex.Number)
+            {
+                case 1017:
+                    LB_Username.ForeColor = Color.Red;
+                    LB_MDP.ForeColor = Color.Red;
+                    TB_Password.Clear();
+                    TB_Password.Focus();
+                    LB_Error.Text = "*Erreur Usager/Mot de passe";
+                    break;
+                case 12170:
+                    MessageBox.Show("La base de données est indisponible,réessayer plus tard", "Erreur 12170", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+                case 12543:
+                    MessageBox.Show("Connexion impossible,Vérifiez votre connection internet", "Erreur 12543", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+                default: MessageBox.Show(Ex.Message.ToString());
+                    break;
+            }
+        }
+    }
+}
