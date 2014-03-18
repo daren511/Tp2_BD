6ef325fb0e2e9eae993dc352a901710aa0b2a46f
hcfranck <hc_throwdown@hotmail.com>
2014-03-17 20:15:01 -0400

    Voila livre ajout - j'ai gagner

--- St-laurent_Daren-ken_ET_Cote_Francis_Tp2_2/Gestion_Livres.cs
+++ St-laurent_Daren-ken_ET_Cote_Francis_Tp2_2/Gestion_Livres.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Oracle.DataAccess.Client;
+using St_laurent_Daren_ken_ET_Cote_Francis_Tp2_2;
 
 namespace St_laurent_Daren_ken_ET_Cote_Francis_Tp2
 {
@@ -31,8 +32,46 @@ namespace St_laurent_Daren_ken_ET_Cote_Francis_Tp2
 
         private void BTN_Ajout_Click(object sender, EventArgs e)
         {
+            Livres_Ajout Ajouter = new Livres_Ajout();
+            Ajouter.conn = this.conn;
+            Ajouter.Text = "Ajout";
+            if (Ajouter.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
 
+                try
+                {
+                    OracleCommand oraAjout = new OracleCommand("GestionRetours", conn);
+                    oraAjout.CommandText = "GestionRetours.Insertion";
+                    oraAjout.CommandType = CommandType.StoredProcedure;
+
+                    OracleParameter OraParaNumLivre = new OracleParameter("PNumLivre", OracleDbType.Int32);
+                    OracleParameter OraParaTitre = new OracleParameter("PTitre", OracleDbType.Varchar2, 40);
+                    OracleParameter OraParaAuteur = new OracleParameter("PAuteur", OracleDbType.Varchar2, 40);
+
+
+                    OraParaNumLivre.Direction = ParameterDirection.Input;
+                    OraParaTitre.Direction = ParameterDirection.Input;
+                    OraParaAuteur.Direction = ParameterDirection.Input;
+
+                    OraParaNumLivre.Value = Ajouter.numLivre;
+                    OraParaTitre.Value = Ajouter.titre;
+                    OraParaAuteur.Value = Ajouter.auteur;
+
+                    oraAjout.Parameters.Add(OraParaNumLivre);
+                    oraAjout.Parameters.Add(OraParaTitre);
+                    oraAjout.Parameters.Add(OraParaAuteur);
+
+                    oraAjout.ExecuteNonQuery();
+                    ReloadDGV();
+                }
+                catch (OracleException ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+
+            }
         }
+        
 
         private void BTN_Modifier_Click(object sender, EventArgs e)
         {
@@ -115,6 +154,31 @@ namespace St_laurent_Daren_ken_ET_Cote_Francis_Tp2
             if(callBackForm != null)
                callBackForm.Show();
             
+        }
+
+        private void ReloadDGV()
+        {
+            try
+            {
+                OracleCommand oraSelect = new OracleCommand("GestionRetours", conn);
+                oraSelect.CommandText = "GestionRetours.Consultlivre";
+                oraSelect.CommandType = CommandType.StoredProcedure;
+
+                //Retour
+                OracleParameter OraParaResultat = new OracleParameter("Resultat", OracleDbType.RefCursor);
+                OraParaResultat.Direction = ParameterDirection.ReturnValue;
+                oraSelect.Parameters.Add(OraParaResultat);
+
+                //Remplir DGV 
+                OracleDataAdapter oraAdapter = new OracleDataAdapter(oraSelect);
+                livreDataSet = new DataSet();
+                oraAdapter.Fill(livreDataSet);
+                DGV_Livres.DataSource = livreDataSet.Tables[0];
+            }
+            catch (OracleException ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
         }
     }
 }
